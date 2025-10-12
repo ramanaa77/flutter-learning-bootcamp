3318b3aac62d926b95c5a331a7df36b40f7a606a
ramanaa77 <ramanaa1010@gmail.com>
2025-10-12 14:57:10 +0530

    Abstraction Process

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'question.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -31,11 +33,6 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List<Question> questionBank = [
-    Question(questionText: 'You can lead a cow down stairs but not up stairs.',questionAnswer: false),
-    Question(questionText: 'Approximately one quarter of human bones are in the feet.', questionAnswer: true),
-    Question(questionText: 'A slug\'s blood is green.', questionAnswer: true)
-  ];
   int questionNumber = 0;
 
   @override
@@ -50,7 +47,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[questionNumber].questionText,
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 25.0, color: Colors.white),
               ),
@@ -71,7 +68,9 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  bool correctAnswer = questionBank[questionNumber].questionAnswer;
+                  // quizBrain.questionBank[questionNumber].questionAnswer = true;   //it change the correct answer, it is not safe so it needs encapsulation.
+
+                  bool correctAnswer = quizBrain.questionBank[questionNumber].questionAnswer;
                   if(correctAnswer == true){
                     print("user got it right.");
                   } else {
@@ -98,7 +97,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked false.
                 setState(() {
-                  bool correctAnswer = questionBank[questionNumber].questionAnswer;
+                  bool correctAnswer = quizBrain.questionBank[questionNumber].questionAnswer;
                   if(correctAnswer == false){
                     print("user got it right.");
                   } else {
