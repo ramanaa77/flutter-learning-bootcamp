a71adaf2e3dc7c0b9577fb9dbafb428b0fcc4189
ramanaa77 <ramanaa1010@gmail.com>
2025-11-03 10:31:19 +0530

    Add geolocator and get location

--- Clima/lib/screens/loading_screen.dart
+++ Clima/lib/screens/loading_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
 
   @override
   State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+void getLocation() async {
+  final LocationSettings locationSettings = LocationSettings(
+    accuracy: LocationAccuracy.best,
+    distanceFilter: 100,
+  );
+  Position position = await Geolocator.getCurrentPosition(locationSettings: locationSettings);
+  print(position);
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
@@ -14,7 +24,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
