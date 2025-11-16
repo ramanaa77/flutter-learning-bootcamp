e41fbdd3b315c6b3b9bfa8aa1a2dcc18b1d58e28
ramanaa77 <ramanaa1010@gmail.com>
2025-11-16 19:46:11 +0530

    Add Routes and Navigator

--- flash_chat/lib/main.dart
+++ flash_chat/lib/main.dart
@@ -17,7 +17,13 @@ class FlashChat extends StatelessWidget {
           bodyMedium: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen()
+      },
     );
   }
 }
--- flash_chat/lib/screens/chat_screen.dart
+++ flash_chat/lib/screens/chat_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/constants.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+  static const String id = 'chat_screen';
 
   @override
   State<ChatScreen> createState() => _ChatScreenState();
