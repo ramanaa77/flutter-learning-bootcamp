695ad6decccd2b076c49ebde0bb93e4d4a2939df
ramanaa77 <ramanaa1010@gmail.com>
2025-12-29 19:10:17 +0530

    widgets refactor

---
+++ todoey/lib/widgets/task_tile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TaskTile extends StatelessWidget {
+  const TaskTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text('This is a task.'),
+      trailing: Checkbox(
+        value: false,
+        onChanged: (bool? value) {},
+      ),
+    );
+  }
+}
---
+++ todoey/lib/widgets/tasks_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:todoey/widgets/task_tile.dart';
+
+class TasksList extends StatelessWidget {
+  const TasksList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        TaskTile(),
+        TaskTile(),
+        TaskTile(),
+      ],
+    );
+  }
+}
