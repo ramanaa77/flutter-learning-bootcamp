58a5a5ac100f905a6dd08871ed7a3ea3d39ef217
ramanaa77 <ramanaa1010@gmail.com>
2026-01-04 13:39:08 +0530

    Checking off To-Do List Tasks with Provider

--- todoey/lib/widgets/tasks_list.dart
+++ todoey/lib/widgets/tasks_list.dart
@@ -12,13 +12,12 @@ class TasksList extends StatelessWidget {
       builder: (context, taskData, child) {
         return ListView.builder(
           itemBuilder: (context, index) {
+            final task = taskData.tasks[index];
             return TaskTile(
-              isChecked: taskData.tasks[index].isDone,
-              taskTitle: taskData.tasks[index].name,
+              isChecked: task.isDone,
+              taskTitle: task.name,
               checkboxCallback: (bool? checkboxState) {
-                // setState(() {
-                //   widget.tasks[index].toggleDone();
-                // });
+                taskData.updateTask(task);
               },
             );
           },
